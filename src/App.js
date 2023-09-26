5895b62a042f7aa9839d3c2eab2affd4333f2b4b
Nadya163 <satara19892202@gmail.com>
2023-09-26 11:59:01 +0600

    Исправление

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-        «Здесь скоро будет Skypro.Music»
+        Здесь скоро будет Skypro.Music
         </p>
       </header>
     </div>
